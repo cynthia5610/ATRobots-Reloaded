c3ae038bbefe05b0941444a0db383038a2c32c53
Unknown <steing@sunyit.edu>
2018-02-20 15:02:23 -0500

    started myfile

---
+++ bin/myfile.cpp
@@ -0,0 +1,44 @@
+/*  MYFILE.PAS
+    Attempted: 2/20/2018
+*/
+
+
+#include <iostream>
+#include <string.h>
+
+
+
+//function prototypes
+void file_size      (FILE myfile);                                //returns filesize
+/*void delete_file  (fn:string);
+void rename_file    (fn,fn2:string);
+void open_filein    (var h:word; fn:string);
+void open_fileout   (var h:word; fn:string);
+void open_fileboth  (var h:word; fn:string);
+void create_fileout (var h:word; fn:string);
+void read_file      (h:word; ploc:pointer; var len:integer);
+void write_file     (h:word; ploc:pointer; var len:integer);
+void close_file     (h:word);
+void file_seek      (h,d:word; m:byte; var p:word);
+void long_seek      (h:word; d:longint; m:byte; var p:longint);
+void read_long      (h:word; ploc:pointer; var len:word);
+void write_long     (h:word; ploc:pointer; var len:word);*/
+
+
+void file_size (FILE fn)
+{
+    
+    
+    
+}
+
+
+
+
+
+int main() 
+{
+    ofstream myfile;
+    file_size(myfile);
+    return 0; 
+}
