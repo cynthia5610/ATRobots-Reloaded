c25719c4dffa265cf676ca9d82671512f54e3f06
Luke Krentz <lukekrentz@gmail.com>
2018-02-22 15:32:10 -0500

    updated func .hh file

--- bin/atrrfunc.hh
+++ bin/atrrfunc.hh
@@ -1,11 +1,30 @@
-#ifndef __ATRRFUNC_CPP_
-#define __ATRRFUNC_CPP_
+#ifndef __ATRRFUNC_HH__
+#define __ATRRFUNC_HH__
 
 #include <iostream>
 #include <stdlib.h>
 
 using namespace std;
 
-extern char hexNum(int number);
+long delay_per_sec;
+bool registered, graphix, sound_on;
+string reg_name;
+unsigned short int reg_num;
+double sint, cost[255];
 
-#endif /* __LEXER_H__ */
+char hexnum(unsigned char num);
+string hexb(unsigned char num);
+string hex(unsigned short num);
+double valuer(string i);
+long value(string i);
+string cstrr(double i);
+string cstr(long i);
+string zero_pad(long n, long l);
+string zero_pads(string s, long l);
+string addfront(string b, int l);
+string uCase(string lString);
+string lcase(string s);
+string space(unsigned char i);
+string repchar(char c, unsigned char i);
+
+#endif
