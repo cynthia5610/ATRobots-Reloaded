37eb68ee2a7b4a01d7cb3c7545b0f868a0d768a3
Justin Perez <justinperez392@gmail.com>
2018-04-03 15:36:01 -0400

    first change to valuer test

--- Test/atrfunc.test/valuer.test2.cpp
+++ Test/atrfunc.test/valuer.test2.cpp
@@ -4,16 +4,18 @@
 #include "../../bin/atrrfunc.hh"
 using namespace Macchiato;
 
-void valuer_test();
+void valuer_test2();
 
-void valuer_test(){
+void valuer_test2(){
     	describe("Testing valuer(char *i)", [&]() {
-			it("i = '10.5'", [&]() {
+			it("i = '1000'", [&]() {
 			    return expect(valuer("10.5")).to->equal(10.5)->getResult();
 			});
 	    });
 
      
-}  	
-   
-
+}   
+int main()
+{
+	std::cout << Macchiato::GetResultantTestOutput() << std::endl;
+}
