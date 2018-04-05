bb4e9cfc3b4011c0cfc67299e8a03acff13b637b
Justin Perez <justinperez392@gmail.com>
2018-04-05 12:36:36 -0400

    renamed filelib folder

--- Test/atrfunc.test/uCase.test.cpp
+++ Test/atrfunc.test/uCase.test.cpp
@@ -5,11 +5,29 @@
 using namespace Macchiato;
 
 void uCase_test();
+char* uCase(char* lString);
+
+char* uCase(char* lString){
+
+	char a[100];
+    while(*lString != '\0'){
+        *lString = toupper((unsigned char)*lString);
+        ++lString;
+    }
+
+    return lString;
+}
 
 void uCase_test(){
     	describe("Testing uCase(string lString)", [&]() {
 			it("lString = 'lower'", [&]() {
-			    return expect(uCase("lower")).to->equal("LOWER")->getResult();
+			    return expect(uCase(0)).to->equal("LOWER")->getResult();
 			});
 	    });
 }   
+
+int main()
+{
+	uCase_test();
+	return 0;
+}
